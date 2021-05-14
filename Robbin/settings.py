1427cb91a2d018ed103d0e80cf8eebbd77b2f031
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-14 15:23:19 +0530

    New SECRET_KEY each time server run

--- Robbin/settings.py
+++ Robbin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pod@-g_3jno7f9+jh-xo7#2!s0rh5qw$a9^edlm%c!5g47q#+*'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
