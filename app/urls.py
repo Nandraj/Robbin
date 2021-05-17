ccede7a4c224c96edd2889bdd2c41a31cd726d06
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-17 18:41:35 +0530

    Templates, Views and Urls added for all funcs and href updated in navbar

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homePage, name='home')
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('org-type/', views.orgType, name='org-type'),
+    path('client/', views.client, name='client'),
+    path('year/', views.year, name='year'),
+    path('period/', views.period, name='period'),
+    path('status/', views.status, name='status'),
+    path('employee/', views.employee, name='employee'),
+    path('assignment/', views.assignment, name='assignment'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
 
 
-def homePage(request):
+def home(request):
     context = {}
     return render(request, 'app/home.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'app/contact.html', context)
+
+
+def orgType(request):
+    context = {}
+    return render(request, 'app/org-type.html', context)
+
+
+def client(request):
+    context = {}
+    return render(request, 'app/client.html', context)
+
+
+def year(request):
+    context = {}
+    return render(request, 'app/year.html', context)
+
+
+def period(request):
+    context = {}
+    return render(request, 'app/period.html', context)
+
+
+def status(request):
+    context = {}
+    return render(request, 'app/status.html', context)
+
+
+def employee(request):
+    context = {}
+    return render(request, 'app/employee.html', context)
+
+
+def assignment(request):
+    context = {}
+    return render(request, 'app/assignment.html', context)
