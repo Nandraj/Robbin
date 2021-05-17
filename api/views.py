2147d96c6e7b5765177554e8b08f02958bdbdeb1
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-17 16:05:09 +0530

    api overview removed as DRF provide it by default

--- api/views.py
+++ api/views.py
@@ -1,8 +1,1 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(['GET'])
-def apiOverView(request):
-    api_urls = {}
-    return Response(api_urls)
+from django.shortcuts import render
