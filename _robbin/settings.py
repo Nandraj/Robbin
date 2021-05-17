67ced64b5e1f3a433be89cd3cb9798032e160133
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-17 16:27:39 +0530

    Api renamed to App and other relevant changed done so that can proceed to build this as full django-app

--- _robbin/settings.py
+++ _robbin/settings.py
@@ -39,15 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
-    'api',
-    'corsheaders',
+    'app',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,7 +126,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000',
-]
