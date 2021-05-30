a238e510c242c3c786db8ee08d7c7afd8b6713a7
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-30 18:14:42 +0530

    nav_bar restructured esp for who logged_in

--- app/decorators.py
+++ app/decorators.py
@@ -9,7 +9,7 @@ def admin_only(view_func):
         group = None
         if request.user.groups.exists():
             group = request.user.groups.first().name
-            if group == 'admin':
+            if group in ['admin', 'Admin']:
                 return view_func(request, *args, **kwargs)
             else:
                 return HttpResponse('<h2>You are not authorized to access this page</h1>')
